6739c7bcb23add5c3ae3ea3f97dc435390f1c7e6
Basak Sarica <saricabasak@gmail.com>
2019-10-01 19:47:18 +0300

    android font bug fixed, hopefully.

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 import { createAppContainer } from "react-navigation";
 import { createMaterialTopTabNavigator } from "react-navigation-tabs";
 import { createStore } from "redux";
@@ -10,7 +13,26 @@ import Navigator from "./navigation/Navigator";
 const store = createStore(reducer);
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
     return (
       <Provider store={store}>
         <PasswordHeader/>

db9044525a34b22169bad78debf89d791fb2eb42
Erkan <erkanerkisi@gmail.com>
2019-10-04 15:58:18 +0300

    Password generation implemented.

--- components/PasswordGenerator.js
+++ components/PasswordGenerator.js
@@ -1,21 +1,12 @@
-let i = 0;
-let j = 0;
-let k = 0;
 let length;
 let digitLength;
 let lowerLength;
 let upperLength;
 let speacialLength;
-let randomLimit;
-let lengthArray = [];
-let password = [];
 
 function generatePassword(request) {
-    console.log('generatePassword - findLengthOfEachType');
     findLengthOfEachType(request);
-    console.log('generatePassword - createPassword');
-    createPassword();
-    return password.toString();
+    return createPassword();
 }
 
 function findLengthOfEachType(request){
@@ -26,29 +17,25 @@ function findLengthOfEachType(request){
   upperLength = request.upperValue ? 1 : 0;
   speacialLength = request.specialValue ? 1 : 0;
 
-  i = getMinLengthOfEachType(length);
-  j = ( digitLength + lowerLength + upperLength + speacialLength);
-  getFinalLengthOfEachType ();
+  let i = getMinLengthOfEachType(length);
+  let j = ( digitLength + lowerLength + upperLength + speacialLength);
+  let lengthArray = getFinalLengthOfEachType (i,j);
 
-  k = 0;
+  let k = 0;
   if (request.digitValue){
     digitLength = lengthArray[k] + i;
     k = k + 1;
-    console.log(JSON.stringify(digitLength));
   }
   if (request.lowerValue){
     lowerLength = lengthArray[k] + i;
     k = k + 1;
-    console.log(JSON.stringify(lowerLength));
   }
   if (request.upperValue){
     upperLength = lengthArray[k] + i;
     k = k + 1;
-    console.log(JSON.stringify(upperLength));
   }
   if (request.specialValue){
     speacialLength = lengthArray[k] + i;
-    console.log(JSON.stringify(speacialLength));
   }
 }
 
@@ -56,69 +43,54 @@ function getMinLengthOfEachType (length) {
   return Math.floor(length/5);
 }
 
-function getFinalLengthOfEachType () {
-  lengthArray = [];
-  randomLimit = length - (i*j);
+function getFinalLengthOfEachType (i,j) {
+  let lengthArray = [];
+  let randomLimit = length - (i*j);
 
-  for(k = 0; k < (j-1); k++){
+  for(let k = 0; k < (j-1); k++){
    lengthArray.push(generateRandom(randomLimit));
    randomLimit = randomLimit - lengthArray[k];
   }
   lengthArray.push(randomLimit);
 
-  console.log('PasswordGenerator - lengthArray :' + JSON.stringify(lengthArray));
-  lengthArray;
+  return lengthArray;
 }
 
 function createPassword () {
   let key;
+  let password = [];
   let digits = [0,1,2,3,4,5,6,7,8,9];
   let lowers = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n'];
   let uppers = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N'];
   let special = ['#','?','+','@','-'];
 
-  for(k = 0; k < digitLength; k++){
-    console.log('k: ' + k );
+  for(let k = 0; k < digitLength; k++){
     key = generateRandom(digits.length-1);
-    console.log('key: ' + key );
     password.push(digits[key]);
   }
-  console.log('PasswordGenerator - createPassword :' + JSON.stringify(password));
 
-  for(k = 0; k < lowerLength; k++){
+  for(let k = 0; k < lowerLength; k++){
     key = generateRandom(lowers.length-1);
     password.push(lowers[key]);
   }
-  console.log('PasswordGenerator - createPassword :' + JSON.stringify(password));
 
-  for(k = 0; k < upperLength; k++){
+  for(let k = 0; k < upperLength; k++){
     key = generateRandom(uppers.length-1);
     password.push(uppers[key]);
   }
-  console.log('PasswordGenerator - createPassword :' + JSON.stringify(password));
 
-  for(k = 0; k < speacialLength; k++){
+  for(let k = 0; k < speacialLength; k++){
     key = generateRandom(special.length-1);
     password.push(special[key]);
   }
-  console.log('PasswordGenerator - createPassword :' + JSON.stringify(password));
 
-  password.sort(function(a, b){return 0.5 - Math.random()});
+  console.log('PasswordGenerator - createPassword :' + JSON.stringify(password));
+  return password.sort(function(a, b){return 0.5 - Math.random()}).join('');
 }
 
 function generateRandom (limit) {
     return Math.floor(Math.random()*limit);
 }
-
-/*
-NOTLAR:
-
-https://www.w3schools.com/js/js_array_sort.asp
-points.sort(function(a, b){return 0.5 - Math.random()});
-
-With clever parameter setting, you can use splice()
-to remove elements without leaving "holes" in the array:
-*/
 
 export default {
   generatePassword

1b6217f0cc0a7f9a2097f2561e36058f409ede97
Erkan <erkanerkisi@gmail.com>
2019-10-04 15:20:57 +0300

    generation fixes

--- components/PasswordGenerator.js
+++ components/PasswordGenerator.js
@@ -75,7 +75,7 @@ function createPassword () {
   let digits = [0,1,2,3,4,5,6,7,8,9];
   let lowers = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n'];
   let uppers = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N'];
-  let special = ['#','?','+'];
+  let special = ['#','?','+','@','-'];
 
   for(k = 0; k < digitLength; k++){
     console.log('k: ' + k );

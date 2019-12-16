6078b8a0ea0d8b6a33d60b1c61ebd37e1664002c
Erkan <erkanerkisi@gmail.com>
2019-12-16 23:23:35 +0300

    swipe list delete icon moved from right side to left side and added disabledleftswipe.

--- components/PasswordItemList.js
+++ components/PasswordItemList.js
@@ -1,62 +1,59 @@
 import React, { Component } from "react";
 import { View, Button, Icon } from "native-base";
 import { connect } from "react-redux";
-import { FlatList } from 'react-native';
+import { FlatList } from "react-native";
 import PasswordItem from "./PasswordItem";
-import { SwipeListView } from 'react-native-swipe-list-view';
+import { SwipeListView } from "react-native-swipe-list-view";
 import { deletePasswordItemArrOnStoreAction } from "../store/actions/PasswordItemAction";
 
 class PasswordItemList extends Component {
-
-  deletePasswordItemDetail = (passwordItem) => {
-      console.log("deletePasswordItemDetail" + JSON.stringify(passwordItem));
-      this.props.deletePasswordItemArrOnStore(passwordItem);
+  deletePasswordItemDetail = passwordItem => {
+    console.log("deletePasswordItemDetail" + JSON.stringify(passwordItem));
+    this.props.deletePasswordItemArrOnStore(passwordItem);
   };
 
   render() {
-    console.log("PasswordItemList - render - " + JSON.stringify(this.props.passwordItems));
+    console.log(
+      "PasswordItemList - render - " + JSON.stringify(this.props.passwordItems)
+    );
     return (
       <SwipeListView
         data={this.props.passwordItems}
-        keyExtractor = {(rowData,index) => {
+        keyExtractor={(rowData, index) => {
           return rowData.id.toString();
         }}
-        renderItem = { (rowData, rowMap) => (
-          <View 
+        renderItem={(rowData, rowMap) => (
+          <View
             style={{
-              backgroundColor: 'white'
+              backgroundColor: "white"
             }}
           >
-            <PasswordItem
-              key = {rowData.item.id}
-              passworditem={rowData.item}
-            />
+            <PasswordItem key={rowData.item.id} passworditem={rowData.item} />
           </View>
         )}
-        renderHiddenItem = { (rowData, rowMap) => (
-          <View style={{
-            flex: 1,
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between'
-          }}>
-            <View/> 
-            <View>
-              <Button 
-                danger 
-                onPress={() => {this.deletePasswordItemDetail(rowData.item)}}
-              >
-                <Icon name="trash" />
-              </Button>
-            </View>
-          </View>
+        renderHiddenItem={(rowData, rowMap) => (
+          <Button
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between"
+            }}
+            danger
+            onPress={() => {
+              this.deletePasswordItemDetail(rowData.item);
+            }}
+          >
+            <Icon name="trash" />
+          </Button>
         )}
-        leftOpenValue = {0}
-        rightOpenValue = {-50}
-        closeOnRowOpen = {true}
-        closeOnRowPress = {true}
-        closeOnScroll = {true}
-        closeOnRowBeginSwipe = {true}
+        leftOpenValue={50}
+        rightOpenValue={0}
+        disableLeftSwipe
+        closeOnRowOpen={true}
+        closeOnRowPress={true}
+        closeOnScroll={true}
+        closeOnRowBeginSwipe={true}
       />
     );
   }
